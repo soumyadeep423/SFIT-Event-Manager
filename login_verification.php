0328b4e179f7b0731a0aa781437126b793254c3a
Soumyadeep <113242256+soumyadeep423@users.noreply.github.com>
2022-10-19 20:00:45 +0530

    Add files via upload

---
+++ login_verification.php
@@ -0,0 +1,26 @@
+<head>
+<meta http-equiv="refresh" content="2; url = flash.html" />
+</head>
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+$con=new mysqli("localhost","root","","sfit");
+if($con->connect_error) {
+die("Failed to connect :".$con->connect_error);
+} else {
+$stmt=$con->prepare("select * from login where username = ?");
+$stmt->bind_param("s",$username);
+$stmt->execute();
+$stmt_result=$stmt->get_result();
+if($stmt_result->num_rows > 0) {
+$data = $stmt_result->fetch_assoc();
+if($data['password']===$password) {
+echo "<h2>Login Successful</h2>";
+} else {
+echo "<h2>Invalid Username or Password</h2>";
+}
+} else {
+echo "<h2>Invalid Username or Password</h2>";
+}
+}
